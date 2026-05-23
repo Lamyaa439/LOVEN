db13057892f0c69f280cca12b53f600bfe322ac0
Yara Alrasheed <11982@holbertonstudents.com>
2026-05-23 23:53:14 +0300

    Fix conflict issues

--- MVP/frontend/loven/lib/core/router/app_router.dart
+++ MVP/frontend/loven/lib/core/router/app_router.dart
@@ -1,25 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:loven/features/auth/controller/cubit/auth_state.dart';
 import 'dart:async';
 
-<<<<<<< HEAD
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:loven/features/artist_profile/controller/artist_profile_cubit.dart';
-import 'package:loven/features/artist_profile/model/artist_repository.dart';
-=======
+import 'package:go_router/go_router.dart';
+
 import 'package:loven/features/auth/controller/cubit/auth_cubit.dart';
->>>>>>> 769f0e1291d83b23ddae30d63ca27cabe596496f
+import 'package:loven/features/auth/controller/cubit/auth_state.dart';
 import 'package:loven/features/auth/view/screens/login_page.dart';
-import 'package:loven/features/artist_profile/model/artist_model.dart';
-import 'package:loven/features/artwork/view/screens/create_artwork_screen.dart';
 import 'package:loven/features/auth/view/screens/profile_screen.dart';
-import 'package:loven/features/splash/splash_screen.dart';
-import 'package:loven/features/home/View/widgets/art_details_screen.dart';
 import 'package:loven/features/auth/view/screens/signup_page.dart';
-import 'package:loven/features/navigation/view/screens/navigation_screen.dart';
+import 'package:loven/features/verification_request/controller/cubit/verification_request_cubit.dart';
+import 'package:loven/features/verification_request/data/repositories/verification_request_repository.dart';
+import 'package:loven/features/artist_profile/controller/artist_profile_cubit.dart';
+import 'package:loven/features/artist_profile/model/artist_model.dart';
+import 'package:loven/features/artist_profile/model/artist_repository.dart';
 import 'package:loven/features/artist_profile/view/screens/artist_profile_screen.dart';
 import 'package:loven/features/artist_profile/view/screens/edit_artist_profile_screen.dart';
+import 'package:loven/features/verification_request/view/screens/verification_request_screen.dart';
+import 'package:loven/features/artwork/view/screens/create_artwork_screen.dart';
+import 'package:loven/features/home/View/widgets/art_details_screen.dart';
+import 'package:loven/features/navigation/view/screens/navigation_screen.dart';
+import 'package:loven/features/splash/splash_screen.dart';
 
 class GoRouterRefreshStream extends ChangeNotifier {
   late final StreamSubscription<dynamic> _subscription;
@@ -48,7 +49,8 @@ GoRouter createRouter(AuthCubit authCubit) {
           path.startsWith('/my-profile') ||
           path.startsWith('/artist') ||
           path.startsWith('/art-details') ||
-          path.startsWith('/artworks/create');
+          path.startsWith('/artworks/create') ||
+          path.startsWith('/verification-request');
 
       if (isGuest && isProtectedPath) {
         return '/auth';
@@ -96,95 +98,36 @@ GoRouter createRouter(AuthCubit authCubit) {
         builder: (context, state) => const CreateArtworkScreen(),
       ),
       GoRoute(
+        path: '/artist-profile/edit',
+        builder: (context, state) {
+          final artist = state.extra as ArtistModel;
+          
+          return BlocProvider(
+            create: (_) => ArtistProfileCubit(
+              repository: ArtistRepository(),
+            ),
+            child: EditArtistProfileScreen(artist: artist),
+          );
+        },
+      ),
+      GoRoute(
+        path: '/verification-request',
+        builder: (context, state) {
+          return BlocProvider(
+            create: (_) => VerificationRequestCubit(
+              VerificationRequestRepository(),
+            ),
+            child: const VerificationRequestScreen(),
+          );
+        },
+      ),
+      GoRoute(
         path: '/art-details',
         builder: (context, state) {
           final extra = state.extra;
           final artItem = extra is ArtworkModel
               ? extra
               : ArtworkModel.fromJson(extra as Map<String, dynamic>);
-
-<<<<<<< HEAD
-    return null;
-  },
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) =>
-          NavigationScreen(isGuest: isUserBrowsingAsGuest),
-    ),
-
-    GoRoute(
-      path: '/auth',
-      builder: (context, state) => const SignupPage(),
-    ),
-    
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginPage(fromGuest: true),
-    ),
-
-    GoRoute(
-      path: '/splash_screen',
-      builder: (context, state) => const SplashScreen(),
-    ),
-
-    GoRoute(
-      path: '/my-profile',
-      builder: (context, state) => const ArtistProfileScreen(),
-    ),
-
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const ArtistProfileScreen(),
-    ),
-
-    GoRoute(
-      path: '/artist/:artistId',
-      builder: (context, state) {
-        final artistId = state.pathParameters['artistId']!;
-        return ArtistProfileScreen(artistProfileId: artistId);
-      },
-    ),
-
-    GoRoute(
-      path: '/cart',
-      builder: (context, state) => const SizedBox.shrink(),
-    ),
-
-    GoRoute(
-      path: '/artworks/create',
-      builder: (context, state) => const CreateArtworkScreen(),
-    ),
-
-    GoRoute(
-      path: '/artist-profile/edit',
-      builder: (context, state) {
-        final artist = state.extra as ArtistModel;
-        return BlocProvider(
-          create: (_) => ArtistProfileCubit(
-            repository: ArtistRepository(),
-          ),
-          child: EditArtistProfileScreen(artist: artist),
-        );
-      },
-    ),
-    
-    GoRoute(
-      path: '/art-details',
-      builder: (context, state) {
-        final extra = state.extra;
-        final artItem = extra is ArtworkModel
-        ? extra
-        : ArtworkModel.fromJson(extra as Map<String, dynamic>);
-        
-        return ArtDetailsScreen(
-          artItem: artItem,
-        );
-      },
-    ),
-  ],
-);
-=======
           return ArtDetailsScreen(
             artItem: artItem,
           );
@@ -193,4 +136,3 @@ GoRouter createRouter(AuthCubit authCubit) {
     ],
   );
 }
->>>>>>> 769f0e1291d83b23ddae30d63ca27cabe596496f
